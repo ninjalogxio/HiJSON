9133095ac6ac4b56672e3cbb1e9da9746e8a2b23
ninjalogxio <xiaolong.jin@icloud.com>
2024-03-02 10:30:05 +0800

    platforms

--- Package.swift
+++ Package.swift
@@ -5,6 +5,11 @@ import PackageDescription
 
 let package = Package(
     name: "HiJSON",
+    platforms: [
+        .iOS(.v16),
+        .macOS(.v13),
+        .watchOS(.v9)
+    ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(name: "HiJSON", targets: ["HiJSON"]),
